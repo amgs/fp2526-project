a12f51339c2afba5e0205fe3b9304b8442518f61
amgs <user@example.com>
2025-11-18 16:58:51 +0000

    Initial commit.

---
+++ Player.cs
@@ -0,0 +1,13 @@
+public class Player {
+    public string Name;
+    public int gamesPlayed;
+    public int wins;
+    public int losses;
+    public int ties;
+    public double money; // TODO: check if this is needed.
+    // FIXME: some fields are missing;
+
+    public Player(string Name) {
+        this.Name = Name;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿public class Program {
+    public static void Main() {
+        Player[] players = [];
+        while (true) {
+            string line = Console.ReadLine();
+            Console.WriteLine(line);
+            string[] commands = line.Split(" ");
+            if (commands[0] == "RJ") {
+                string Name = commands[1];
+                bool found = false;
+                foreach(Player p in players) {
+                    if(p.Name == Name) {
+                        Console.WriteLine("Jogador existente.");
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) {
+                    // Registar o jogador.
+                    players.Append(new Player(Name));
+                    Console.WriteLine("Jogador registado com sucesso.");
+                }
+            } else if (commands[0] == "LJ") {
+
+            } else if (commands[0] == "IJ") {
+
+            } else if (commands[0] == "LD") {
+
+            } else if (commands[0] == "CE") {
+
+            } else if (commands[0] == "DJ") {
+
+            } else if (commands[0] == "TT") {
+
+            } else if (commands[0] == "PA") {
+
+            } else if (commands[0] == "CC") {
+
+            } else if (commands[0] == "TC") {
+
+            }
+        }
+    }
+}
