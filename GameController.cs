832557fc13c00ec341f6fed2add2a7c5712bf2cd
amgs <user@example.com>
2025-11-19 18:04:31 +0000

    Change player collection to Dictionary.

--- GameController.cs
+++ GameController.cs
@@ -1,12 +1,12 @@
 public class GameController {
-    private Player[] players = [];
+    private Dictionary<string,Player> players = new Dictionary<string,Player>();
     public bool HasPlayer(string playerName) {
-        foreach(Player player in players) {
-            if(player.Name == playerName) {
-                return true;
-            }
-        }
-        return false;
+        // foreach(Player player in players) {
+        //     if(player.Name == playerName) {
+        //         return true;
+        //     }
+        // }
+        return players.Keys.Contains(playerName);
     }
 
     public void RegisterPlayer(string playerName){
